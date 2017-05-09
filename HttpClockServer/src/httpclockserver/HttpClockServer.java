272aad326458962f3d39cb38d7ec897b0a30862c
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-05-09 11:10:38 +0200

    init server

---
+++ HttpClockServer/src/httpclockserver/HttpClockServer.java
@@ -0,0 +1,13 @@
+package httpclockserver;
+
+
+public class HttpClockServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
