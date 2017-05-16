12dd1439d65ba1262256af587dca1f2ab410513f
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-05-16 09:22:21 +0200

    Server side qui ne fonctionne pas du tout

--- HttpClockServer/src/httpclockserver/HttpClockServer.java
+++ HttpClockServer/src/httpclockserver/HttpClockServer.java
@@ -1,13 +1,66 @@
 package httpclockserver;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class HttpClockServer {
+    
+    private int port;
+    private HttpServer server;
+    
+    public HttpClockServer() throws IOException{
+        this(80);
+    }
+    
+    public HttpClockServer(int port) throws IOException{
+        this.port = port;
+        
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        
+        server.createContext("/", new HourHandler());
+    }
+    
+    
+    public void start(){
+        server.start();
+    }
+    
+    private static class HourHandler implements HttpHandler {
+
+        public HourHandler() {
+            
+        }
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String content = he.getRequestHeaders().getFirst("Content-Type");
+            
+            he.sendResponseHeaders(200, content.length());
+            OutputStream os = he.getResponseBody();
+            os.write(content.getBytes());
+            os.close();
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            HttpClockServer httpClockServer = new HttpClockServer(80);
+            httpClockServer.start();
+        } catch (IOException ex) {
+            Logger.getLogger(HttpClockServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+
     
 }
